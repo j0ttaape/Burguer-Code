9c0fbee4ac5638c1ce08867b5fb1e67c1cb54ee7
j0ttaape <joaopedrotrajano59@gmail.com>
2025-06-20 20:08:23 -0300

    Ajustando opcoes
    algumas funcoes estao com erro aidna

--- app/app.js
+++ app/app.js
@@ -6,22 +6,31 @@ let resp;
 
 
 while(resp!==0){
-    await funcoes.sleep(2000);
+        
 funcoes.ApresentarPrograma();
     
     await funcoes.sleep(2000);
 
 console.log(`\n\n---------------------------------------------------------------\n O que iremos fazer? \n---------------------------------------------------------------\n`)
+    await funcoes.sleep(3000);    
+
     resp = Number(ler());
 
     await funcoes.sleep(2000);
 
     if(resp==1){
+        funcoes.cardapio();
+    }
+
+    else if(resp==2){
         funcoes.RegistroDeVenda(resp);
     }
     
-    else if(resp==2){
+    else if(resp==3){
         funcoes.RemoverVenda(resp);
+    }
+    else if(resp==4){
+        funcoes.ListarRegistros()
     }
     
 }
--- app/funcoes.js
+++ app/funcoes.js
@@ -16,38 +16,92 @@ export function sleep(ms) {
 
 //Apresentacao do programa
 export function ApresentarPrograma (){
-    console.log(`---------------------------------------------------------------\n      Olá, seja bem-vindo a BurguerCode \n   O que você deseja fazer? \n   1. Registrar Venda \n   2. Cancelar Venda \n   3. Listar vendas \n   4. Listar vendas de um periodo \n   5. Apresentar Cardapio \n   0. Sair do programa\n---------------------------------------------------------------`)
+    console.log(`---------------------------------------------------------------\n   Olá, seja bem-vindo a BurguerCode \n   O que você deseja fazer? \n  1. Apresentar Cardapio \n   2. Registrar Venda \n   3. Cancelar Venda \n   4. Listar vendas \n   5. Soma total das vendas \n   0. Sair do programa\n---------------------------------------------------------------`)
+}
+
+//funcao do cardapio com preço
+export async function cardapio() {
+    await sleep(2000);
+    console.log(`---------------------------------------------------------------\n   Opções: \n   Sandubão.......... R$12\n   CheeseBurgao.......... R$17,50\n   BurgaoDuplo.......... R$19\n   Coca-Cola.......... R$11\n   HamburguerVegetariano.......... R$15`)
 }
 
 // funcao de registro
-export function RegistroDeVenda(resp){
+export async function RegistroDeVenda(resp){
+    await sleep(2000);  
+
     console.log(`---------------------------------------------------------------\n Boa! você escolheu registrar uma venda \n Qual a posição do lanche pedido? \n--------------------------------------------------------------- `)
     let lanche = Number(ler());
 
+    console.log(`Qual a quantidade que você irá registrar?`)
+    let qtd = Number(ler());
+
     if(lanche == 0){
         console.log(`Registrei a venda de um Sandubão!`)
-        Cardapio[0].quantidade+= 1;
+        Cardapio[0].quantidade+= qtd;
     }
     else if(lanche==1){
         console.log(`Registrei a venda de um CheeseBurguer`)
-        Cardapio[1].quantidade+= 1;
+        Cardapio[1].quantidade+= qtd;
     }
     else if(lanche==2){
         console.log(`Registrei a venda de um Coca-Cola`)
-        Cardapio[2].quantidade+= 1;
+        Cardapio[2].quantidade+= qtd;
     }
     
     else if(lanche==3){
         console.log(`Registrei a venda de um HamburguerVegetariano`)
-        Cardapio[3].quantidade+= 1;
+        Cardapio[3].quantidade+= qtd;
     }
     else(console.log(`Opcão Inválida, digite de novo!`))
 
 }
 
 //funcao de remover registro
-export function RemoverVenda(resp){
+export async function RemoverVenda(resp, Cardapio){
 
-    console.log(`\n\n---------------------------------------------------------------\n Qual a posição `)
-    
+    await sleep(2000);
+
+    console.log(`\n\n---------------------------------------------------------------\n Qual vai ser o registro que você deseja remover\n   0.Sandubão \n   1.CheeseBurguer \n   2.Coca-Cola \n   3.HamburguerVegetariano   ---------------------------------------------------------------?`)
+    let remover = Number(ler());
+
+    await sleep(2000);
+
+    console.log(`Qual a quantidade?`)
+    let qtd = Number(ler());
+
+    await sleep(2000);
+
+    if(remover>=0 && remover <= 3){
+ 
+        if(remover === 0){
+        Cardapio[0].quantidade -= qtd;
+        console.log(`Removi ${qtd} de Sandubão, agora você possui ${Cardapio[0].quantidade}`)
+        }
+        else if(remover === 1){
+            Cardapio[1].quantidade -= qtd;
+            console.log(`Removi ${qtd} de CheeseBurguer, agora você possui ${Cardapio[1].quantidade}`)
+
+        }
+        
+        else if(remover === 2){
+            Cardapio[2].quantidade -= qtd;
+            console.log(`Removi ${qtd} de Coca-Cola, agora você possui ${Cardapio[2].quantidade}`)
+
+        }
+        
+        else if(remover === 3){
+            Cardapio[3].quantidade -= qtd;
+            console.log(`Removi ${qtd} de HamburguerVegetariano, agora você possui ${Cardapio[3].quantidade}`)
+
+        }
+    }
+    else(console.log(`Opção inválida`))
+}
+
+//Listar registros dos itens 
+
+export async function ListarRegistros(Cardapio) {
+    await sleep(3000);
+
+    console.log(`---------------------------------------------------------------\n   Quantidade de Sandubão = ${Cardapio[0].quantidade}\n   Quantidade de CheeseBurguer = ${Cardapio[1].quantidade}\n   Quantidade de Coca-Cola = ${Cardapio[2].quantidade}\n  Quantidade de HamburguerVegetariano = ${Cardapio[3].quantidade}\n         `)
 }
