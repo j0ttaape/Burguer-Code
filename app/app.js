6a652939c94dd1067cecadcf2859ef83b3806bf4
j0ttaape <joaopedrotrajano59@gmail.com>
2025-06-20 17:43:05 -0300

    iniciando a opcao 2

--- app/app.js
+++ app/app.js
@@ -1,11 +1,27 @@
 import * as funcoes from './funcoes.js'
-let resposta;
+import prompt from 'prompt-sync'
+let ler = prompt();
+let opcao = 0;
+let resp;
 
 
+while(resp!==0){
+    await funcoes.sleep(2000);
 funcoes.ApresentarPrograma();
+    
+    await funcoes.sleep(2000);
 
-funcoes.RespostadoUser(resposta);
+console.log(`\n\n---------------------------------------------------------------\n O que iremos fazer? \n---------------------------------------------------------------\n`)
+    resp = Number(ler());
 
-for(let i = 1; i != 0; i ++){
-    funcoes.RegistroDeVenda(resposta);
+    await funcoes.sleep(2000);
+
+    if(resp==1){
+        funcoes.RegistroDeVenda(resp);
+    }
+    
+    else if(resp==2){
+        funcoes.RemoverVenda(resp);
+    }
+    
 }
--- app/funcoes.js
+++ app/funcoes.js
@@ -9,16 +9,14 @@ let Cardapio = [
 ]
 
 
+//funcao sleep
+export function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 //Apresentacao do programa
 export function ApresentarPrograma (){
     console.log(`---------------------------------------------------------------\n      Olá, seja bem-vindo a BurguerCode \n   O que você deseja fazer? \n   1. Registrar Venda \n   2. Cancelar Venda \n   3. Listar vendas \n   4. Listar vendas de um periodo \n   5. Apresentar Cardapio \n   0. Sair do programa\n---------------------------------------------------------------`)
-}
-
-export function RespostadoUser(){
-    
-    console.log(`\n\n---------------------------------------------------------------\n O que iremos fazer? \n---------------------------------------------------------------\n`)
-    resp = Number(ler());
-    return resp
 }
 
 // funcao de registro
@@ -26,20 +24,20 @@ export function RegistroDeVenda(resp){
     console.log(`---------------------------------------------------------------\n Boa! você escolheu registrar uma venda \n Qual a posição do lanche pedido? \n--------------------------------------------------------------- `)
     let lanche = Number(ler());
 
-    if(lanche === 0){
+    if(lanche == 0){
         console.log(`Registrei a venda de um Sandubão!`)
         Cardapio[0].quantidade+= 1;
     }
-    else if(lanche===1){
+    else if(lanche==1){
         console.log(`Registrei a venda de um CheeseBurguer`)
         Cardapio[1].quantidade+= 1;
     }
-    else if(lanche===2){
+    else if(lanche==2){
         console.log(`Registrei a venda de um Coca-Cola`)
         Cardapio[2].quantidade+= 1;
     }
     
-    else if(lanche===3){
+    else if(lanche==3){
         console.log(`Registrei a venda de um HamburguerVegetariano`)
         Cardapio[3].quantidade+= 1;
     }
@@ -47,3 +45,9 @@ export function RegistroDeVenda(resp){
 
 }
 
+//funcao de remover registro
+export function RemoverVenda(resp){
+
+    console.log(`\n\n---------------------------------------------------------------\n Qual a posição `)
+    
+}
